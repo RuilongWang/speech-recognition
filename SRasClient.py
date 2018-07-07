da73c2903bab64ba7b110e9ab4bec7db96ddc085
RuilongWang <40914090+RuilongWang@users.noreply.github.com>
2018-07-07 13:47:36 +0200

    Update SRasClient.py

--- SRasClient.py
+++ SRasClient.py
@@ -5,7 +5,9 @@ import snowboydecoder
 import json
 
 interrupted = False
+#tornado websocket client clss
 class Client(object):
+    #initialize
     def __init__(self, url, timeout):
         self.url = url
         self.timeout = timeout
@@ -16,6 +18,7 @@ class Client(object):
         self.ioloop.start()
 
     @gen.coroutine
+    #on connection giving prompt
     def connect(self):
         print("trying to connect")
         try:
@@ -24,10 +27,10 @@ class Client(object):
             print ("connection error")
         else:
             print ("connected")
-            self.listen()
+            self.listen()       #doing function listen when connected
 
     switch = 0
-    # print(messages)
+    # set language models and their sensitivity
     models = ["resources/ellbogen_mehr.pmdl", "resources/ellbogen_weniger.pmdl", "resources/hand_mehr.pmdl",
               "resources/hand_weniger.pmdl", "resources/halt.pmdl", "resources/stop.pmdl"]
     sensitivity = [0.47, 0.5, 0.43, 0.43, 0.4, 0.49]
@@ -35,16 +38,16 @@ class Client(object):
 
     def open(self):
         pass
-
+    # interrupt the recording when interrupted = True
     def interrupt_callback(self):
         global interrupted
         return interrupted
-
+    # do callback funtions when hotword detected
     def callback1(self):
         snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
         command = {'command': 1}
-        message = json.dumps(command)
-        self.ws.write_message(message)
+        message = json.dumps(command)# make command to Json format
+        self.ws.write_message(message)#send message to websocket
         print("command = ", message)
 
     def callback2(self):
@@ -74,17 +77,16 @@ class Client(object):
         message = json.dumps(command)
         self.ws.write_message(message)
         print("command = ", message)
-
+    #callback6: when hotword "terminate" detected make interrupted = True, stop recording
     def callback6(self):
         snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
         global interrupted
         interrupted = True
         self.ws.write_message("terminate")
         print("terminate")
-
+        
+    #listen function(snowboy)
     def listen(self):
-
-        # print(self.on_message("1"))
         callbacks = [self.callback1, self.callback2, self.callback3, self.callback4, self.callback5, self.callback6]
 
         print("say something")
@@ -94,7 +96,7 @@ class Client(object):
         print("terminate")
         self.detector.terminate()
 
-
+    #tornado frame, no use---
     @gen.coroutine
     def run(self):
         while True:
@@ -109,7 +111,8 @@ class Client(object):
             self.connect()
         else:
             self.ws.write_message("keep alive")
-
+            
+#websocket adress
 if __name__ == "__main__":
     client = Client("ws://172.20.10.10:4233", 5)
     #client = Client("ws://localhost:1234", 5)
